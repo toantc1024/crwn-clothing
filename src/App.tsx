1d681997c7ebabaf11a660ae10c820d53c293788
Toan <thanhnoobs@gmail.com>
2023-05-04 20:56:49 +0700

    TYPESCRIPT

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Authentication from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
-import { checkUserSession, setCurrentUser } from "./store/user/user.action";
+import { checkUserSession } from "./store/user/user.action";
 import { setIsCartOpen } from "./store/cart/cart.action";
 
 const App = () => {
